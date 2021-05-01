652f3460a8908bcd1307dc392436dfe0b2fa5947
Tomáš Trejdl <tom.trejdl@seznam.cz>
2021-05-01 10:48:24 +0200

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vizualization of electromagnetic field near a black hole
+
+FEL ČVUT 2021
+
+Author: Tomáš Trejdl
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'troisjs'
+import App from './App.vue'
+import './assets/tailwind.css'
+
+createApp(App).mount('#app')
